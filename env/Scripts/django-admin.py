9abd9810fff615b37cbb4b9c5877370e6b7ace77
dhargrefiel <digrefiel2020@plm.edu.ph>
2022-05-02 23:50:31 +0800

    Venv Folder
    I added another environment since env doesn't work on my PC.

--- env/Scripts/django-admin.py
+++ env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\vea\desktop\iplm-master\env\scripts\python.exe
+#!c:\users\pc\desktop\iplm-master-final\env\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
